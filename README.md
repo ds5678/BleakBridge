53ff5f547375a1f029d93a6505bf4843a5f74ea0
ds5678 <jrpritts7@gmail.com>
2021-01-28 19:59:06 -0500

    Add project files.

---
+++ README.md
@@ -0,0 +1,9 @@
+﻿# BleakBridge
+
+This mod for *The Long Dark* creates a bridge allowing you to walk to the Cannery Workshop.
+
+## Installation
+
+1. If you haven't done so already, install MelonLoader by downloading and running [MelonLoader.Installer.exe](https://github.com/HerpDerpinstine/MelonLoader/releases/latest/download/MelonLoader.Installer.exe)
+2. Download the latest version of `BleakBridge.dll` from the [releases page](https://github.com/ds5678/BleakBridge/releases)
+3. Move `BleakBridge.dll` into the Mods folder in your TLD install directory
---
+++ src/Patches.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Harmony;
+using UnityEngine;
+
+namespace BleakBridge
+{
+    internal class Patches
+    { 
+        [HarmonyPatch(typeof(SceneManager),"OnSceneLoaded")]
+        internal static class SpawnBridge
+        {
+            private static void Postfix()
+            {
+                if(GameManager.m_ActiveScene == "CanneryRegion")
+                {
+                    BlockUtils.createBlocks();
+                    MelonLoader.MelonLogger.Log("Block Bridge Spawned");
+                }
+            }
+        }
+    }
+}
